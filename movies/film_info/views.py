af7b9cc327387f47b683e78ec8ea07ef3afb3f58
Hasmita Patel <hasmitapatel@Hasmitas-MacBook-Pro.local>
2019-02-12 01:47:06 +0000

    created admin superuser and added films

--- movies/film_info/views.py
+++ movies/film_info/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def homepage(request):
-    return HttpResponse ("<h2>testing</h2>")
+    return HttpResponse ("<h2>MOVIES</h2>")
