cddaa24cb5128a493e5cfa6cd91c6c6cf39f76f7
Hasmita Patel <hasmitapatel@Hasmitas-MacBook-Pro.local>
2019-02-12 22:29:18 +0000

    username, messages

--- movies/film_info/urls.py
+++ movies/film_info/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(r'', views.homepage, name='homepage')]
+    path(r'', views.homepage, name='homepage'),
+    path(r'register/', views.register, name='register')]
--- movies/film_info/views.py
+++ movies/film_info/views.py
@@ -1,8 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.http import HttpResponse
 
 from .models import Film
 
+from django.contrib.auth.forms import UserCreationForm
+
+from django.contrib.auth import login, logout, authenticate
+
+from django.contrib import messages
+
+
 def homepage(request):
     return render(request=request, template_name="film_info/home.html", context={"all_movies": Film.objects.all()})
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username= form.cleaned_data.get('username')
+            messages.success(request, f"New Account Created: {username}")
+            login(request, user)
+            return redirect("film_info:homepage")
+        else:
+            for msg in form.error_messages:
+                messages.error(request, f"{msg}: {form.error_messages[msg]}")
+
+    form = UserCreationForm
+    return render(request=request, template_name="film_info/register.html", context={"form": form})
