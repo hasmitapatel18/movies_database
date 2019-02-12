222e56c3d1d22a3c4a61eede08c413201864beec
Hasmita Patel <hasmitapatel@Hasmitas-MacBook-Pro.local>
2019-02-12 00:54:32 +0000

    initial commit; django webapp, models, migrations

---
+++ movies/film_info/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FilmInfoConfig(AppConfig):
+    name = 'film_info'
---
+++ movies/film_info/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Film(models.Model):
+    film_title=models.CharField(max_length=200)
+    year = models.PositiveSmallIntegerField(blank=True, null=True)
+    genre=models.CharField(max_length=100)
+    review=models.TextField()
+
+    def __str__(self):
+        return self.film_title
